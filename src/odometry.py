56c7e885b4bac421e17cf0d4edf7ca6c1c643863
Hyronnimus <f-thiessenhusen@posteo.de>
2019-03-15 13:23:32 +0100

    PD-Controller: first tests

--- src/odometry.py
+++ src/odometry.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
+from ev3dev.ev3 import *
 import time
 def drive():
-    m = ev3.LargeMotor("outB")
-    m.run_timed(time_sp=3000, speed_sp=500)
-    time.sleep(5000)
+	mr = ev3.LargeMotor("outC")
+	ml = ev3.LargeMotor("outB")
+	cs = ev3.ColorSensor()
+	cs.mode = "RGB-RAW"
+	while True:
+		#print(cs.bin_data("hhh"))
+		colorsum = cs.bin_data("hhh")[0] + cs.bin_data("hhh")[1] + cs.bin_data("hhh")[2]
+		print(colorsum)
+		a = 250 - colorsum
+		if colorsum > 250:
+			mr.speed_sp = a
+			ml.speed_sp = 100
+		else:
+			mr.speed_sp = a
+			ml.speed_sp = 100
+		#mr.command = "run-forever"
+		#ml.command = "run-forever"
+		time.sleep(0.1)
+		if colorsum > 250:
+			#mr.stop()
+			#ml.stop()
+			break
 
 #  Suggestion: 	implement odometry as class that is not using the ev3dev.ev3 package
 # 				establish value exchange with main driving class via getters and setters
